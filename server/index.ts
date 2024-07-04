b32ab1e6ec9c783178f7740cf7fd78e4e3197399
Ren Hongyi <hongyi.ren@seamoney.com>
2024-07-04 16:04:34 +0800

    fix: fix cors

--- server/index.ts
+++ server/index.ts
@@ -8,14 +8,9 @@ const compression = require('compression');
 
 const app = express();
 const port = 8080;
-const corsOptions = {
-    origin: 'https://hongyi.vercel.app', // Allow only your frontend domain
-    methods: ['GET', 'POST', 'OPTIONS'], // Allow specific methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Preflight OPTIONS request
+
+// Enable CORS with detailed options
+app.use(cors());
 
 app.use(compression());
 
@@ -27,46 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-
-// Handle GET requests to /api route
-app.post('/api/send-email', (req, res) => {
-    const { name, company, email, message } = req.body;
-
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        auth: {
-            user: process.env.FOLIO_EMAIL,
-            pass: process.env.FOLIO_PASSWORD,
-        },
-    });
-
-    transporter
-        .verify()
-        .then(() => {
-            transporter
-                .sendMail({
-                    from: `"${name}" <hongyiren.job@gmail.com>`, // sender address
-                    to: 'hongyiren.job@gmail.com', // list of receivers
-                    subject: `${name} <${email}> ${
-                        company ? `from ${company}` : ''
-                    } submitted a contact form`, // Subject line
-                    text: `${message}`, // plain text body
-                })
-                .then((info) => {
-                    console.log({ info });
-                    res.json({ message: 'success' });
-                })
-                .catch((e) => {
-                    console.error(e);
-                    res.status(500).send(e);
-                });
-        })
-        .catch((e) => {
-            console.error(e);
-            res.status(500).send(e);
-        });
-});
 
 // listen to app on port 8080
 app.listen(port, () => {
